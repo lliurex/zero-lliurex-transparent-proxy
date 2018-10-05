3d57bb28e83bdc5b5d0c1094a33739aa6991633f
juanma1980 <juanma1980@gmail.com>
2018-10-05 13:02:49 +0200

    Create debian/bionic

---
+++ lliurex-po/generate_po.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PYTHON_FILES="../install-files/usr/sbin/transparent-proxy-manager.py"
+
+mkdir -p zero-lliurex-transparent-proxy/
+
+xgettext $PYTHON_FILES -o zero-lliurex-transparent-proxy/zero-lliurex-transparent-proxy.pot
+
